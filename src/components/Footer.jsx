391ce86e6699eaedcf8a7fb97f5e46891aa108b9
Shaik NagulShareef <sns7661907634@gmail.com>
2025-04-23 22:18:17 +0530

    in this commit i added some changes related to logos and adding correct address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
@@ -26,8 +25,8 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-212-456-7890</li>
-                <li>contact@foreveryou.com</li>
+                <li>7095073538</li>
+                <li>mensfashions@gmail.com</li>
             </ul>
         </div>
 
@@ -35,7 +34,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2025@ mensfashions.in - All Right Reserved.</p>
         </div>
 
     </div>
